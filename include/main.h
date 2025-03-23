26e6a05c03094b7d46add11d2ad5f17842bc813e
willbowden <willbowden03@yahoo.co.uk>
2025-03-23 11:54:21 +0800

    Basic game state machine with game pause function

--- include/main.h
+++ include/main.h
@@ -4,11 +4,22 @@
 #include "entity_system.h"
 #include "player.h"
 
+typedef enum GameState {
+  LOADING,
+  PLAYER_TURN,
+  ENEMY_TURN,
+  PAUSED,
+  MENU_OPEN,
+  DIALOG_OPEN
+} GameState;
+
+
 typedef struct Game
 {
   GraphicsEngine *graphics;
   Level *level;
   Player *player;
+  GameState state;
 } Game;
 
 void cleanup_game(Game *game);
